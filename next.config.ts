3d8900af3b5835fcbe4f17480f3e7c6c27a2074a
nishan1805 <76156501+nishan1805@users.noreply.github.com>
2026-01-12 21:48:52 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    domains: ['xnerjzonuekqznphrkjs.supabase.co'],
+    domains: ['https://udehchhzeekvunyuyevd.supabase.co'],
   },
 };
 
