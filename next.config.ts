d271b498555e57be9074e4d2ff2899d07d88bb65
nishan1805 <76156501+nishan1805@users.noreply.github.com>
2026-01-12 21:54:27 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,15 @@
-import type { NextConfig } from 'next';
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
-    domains: ['https://udehchhzeekvunyuyevd.supabase.co'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'udehchhzeekvunyuyevd.supabase.co',
+        pathname: '/storage/v1/object/public/**',
+      },
+    ],
   },
-};
+}
 
-export default nextConfig;
+export default nextConfig
